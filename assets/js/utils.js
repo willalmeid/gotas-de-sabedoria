19e6f03e5008add2df2714053d6dcd4ac0878662
willalmeid <almeida.william0208@gmail.com>
2025-08-16 17:36:14 -0300

    refactor: change file paths

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,6 +1,6 @@
 export async function fetchQuotes() {
 	try {
-		const response = await fetch('assets/data/quotes.json');
+		const response = await fetch('/gotas-de-sabedoria/assets/data/quotes.json');
 		if (!response.ok) {
 			throw new Error('Erro ao carregar o arquivo quotes.json');
 		}
